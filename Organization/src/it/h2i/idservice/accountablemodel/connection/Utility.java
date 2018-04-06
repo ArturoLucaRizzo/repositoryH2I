d9508add51fbe310179a6534e2926e9d0b244ebf
alfino990 <amira@DESKTOP-03SGER4>
2018-04-06 11:12:52 +0200

    change mail

--- Organization/src/it/h2i/idservice/accountablemodel/connection/Utility.java
+++ Organization/src/it/h2i/idservice/accountablemodel/connection/Utility.java
@@ -43,6 +43,7 @@ public class Utility {
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
 		Session session = Session.getInstance(props,
 				new javax.mail.Authenticator() {
